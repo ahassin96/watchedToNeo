8b5849e4d09ee29bfaf4d63f790de0470bc00cba
Alistair Hassin <alistairh96@outlook.com>
2024-01-14 18:35:08 +0000

    commit 11

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 from flask_cors import CORS
+import logging
 import os
 
 load_dotenv()
@@ -9,6 +10,8 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 uri = os.getenv("NEO4J_URI", "bolt://ec2-54-88-88-30.compute-1.amazonaws.com:7687")
 username = os.getenv("NEO4J_USERNAME", "default_username")
@@ -31,6 +34,7 @@ def watched_video():
             request.json.get('user_profile'),
             request.json.get('video_id')
         )
+        logging.debug(f"Received data - user_id: {user_id}, user_profile: {user_profile}, video_id: {video_id}")
 
         if not all((user_id, user_profile, video_id)):
             return make_response(jsonify({'success': False, 'error': 'Missing required parameters'}), 400)
@@ -38,7 +42,7 @@ def watched_video():
         with GraphDatabase.driver(uri, auth=(username, password)) as driver:
             with driver.session() as session:
                 session.write_transaction(create_watched_relation, user_id, video_id)
-
+        logging.info("Watched video recorded successfully")
         response_data = {
             'success': True,
             'message': 'Watched video recorded successfully',
@@ -49,6 +53,7 @@ def watched_video():
         return make_response(jsonify(response_data), 201)
 
     except Exception as e:
+        logging.error(f"Error: {str(e)}")
         return make_response(jsonify({'success': False, 'error': str(e)}), 500)
 
 if __name__ == '__main__':
