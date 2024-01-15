d5f09611c0d507c39e36edee8b521e2db77db0c0
Alistair Hassin <alistairh96@outlook.com>
2024-01-15 11:27:23 +0000

    commit 17

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from neo4j import GraphDatabase
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
-import json
 
 load_dotenv()
 
@@ -15,7 +14,7 @@ username = os.getenv("NEO4J_USERNAME", "default_username")
 password = os.getenv("NEO4J_PASSWORD", "default_password")
 
 
-def create_watched_relation(tx, user_id, user_profile, video_id, tags):
+def create_watched_relation(tx, user_id, user_profile, video_id):
     check_user_query = "MATCH (u:User {user_id: $user_id}) RETURN u"
     result = tx.run(check_user_query, user_id=user_id)
 
@@ -25,45 +24,42 @@ def create_watched_relation(tx, user_id, user_profile, video_id, tags):
 
     create_watched_query = (
         "MATCH (u:User {user_id: $user_id}) "
-        "CREATE (v:Video {video_id: $video_id, tags: $tags}) "
+        "CREATE (v:Video {video_id: $video_id}) "
         "CREATE (u)-[:WATCHED]->(v)"
     )
 
-    tx.run(create_watched_query, user_id=user_id, video_id=video_id, tags=tags)
+    tx.run(create_watched_query, user_id=user_id, video_id=video_id)
 
 
 @app.route('/watched', methods=['POST'])
 def watched_video():
     try:
-        user_id, user_profile, video_id, tags = (
+        user_id, user_profile, video_id = (
             request.json.get('user_id'),
             request.json.get('user_profile'),
-            request.json.get('video_id'),
-            request.json.get('tags', [])
+            request.json.get('video_id')
         )
 
         if not all((user_id, user_profile, video_id)):
             return make_response(jsonify({'success': False, 'error': 'Missing required parameters'}), 400)
 
-        tags = json.loads(tags)
-
         with GraphDatabase.driver(uri, auth=(username, password)) as driver:
             with driver.session() as session:
-                session.write_transaction(create_watched_relation, user_id, user_profile, video_id, tags)
+                session.write_transaction(create_watched_relation, user_id, user_profile, video_id)
 
         response_data = {
             'success': True,
             'message': 'Watched video recorded successfully',
             'user_id': user_id,
             'user_profile': user_profile,
-            'video_id': video_id,
-            'tags': tags
+            'video_id': video_id
         }
 
         return make_response(jsonify(response_data), 201)
 
     except Exception as e:
         return make_response(jsonify({'success': False, 'error': str(e)}), 500)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9091, debug=True)
